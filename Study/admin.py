e8a36ff9ba7e61f617923128ba4529a82f454e6a
Dinar5 <>
2019-02-14 21:49:35 +0400

    some fix

--- Study/admin.py
+++ Study/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from .models import UserProfile,Lesson,Comment
 
 # Register your models here.
+
 admin.site.register(Lesson)
 admin.site.register(Comment)
 admin.site.register(UserProfile)
