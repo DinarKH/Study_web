1052f9d2b1af34d55695f0a178e209e13bedafc4
Dinar5 <>
2019-02-25 17:24:07 +0400

    format code

--- Study/views.py
+++ Study/views.py
@@ -1,29 +1,22 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import AuthenticationForm, UserCreationForm, PasswordChangeForm
-from django.contrib.auth import authenticate,login,update_session_auth_hash
+from django.contrib.auth import authenticate, login, update_session_auth_hash
 from .forms import RegistrationForm
-# from .models import Article
-from django.http import HttpResponse
+
 
 
 def home(request):
-    return render(request, 'home.html',)
+    return render(request, 'home.html', )
 
 
 def articles(request):
     return render(request, 'login.html')
-    # article_list=Article.objects.all()
-    # return render(request, 'articles.html',
-    #               {
-    #                   'articles':article_list,
-    #               })
-
 
 def loginView(request):
     return render(request, 'login.html',
-        {
-        'form': AuthenticationForm
-    })
+                  {
+                      'form': AuthenticationForm
+                  })
 
 
 def registration(requst):
@@ -31,9 +24,9 @@ def registration(requst):
         form = RegistrationForm(requst.POST)
         if form.is_valid():
             form.save()
-            new_user=authenticate(requst,username=form.cleaned_data['username'],
-                                  password=form.cleaned_data['password1'])
-            login(requst,new_user)
+            new_user = authenticate(requst, username=form.cleaned_data['username'],
+                                    password=form.cleaned_data['password1'])
+            login(requst, new_user)
             return redirect('/home/')
         else:
             return render(requst, 'registration.html', {
@@ -44,17 +37,18 @@ def registration(requst):
             'form': RegistrationForm,
         })
 
+
 def changePassword(request):
-    if request.method=='POST':
-        form =PasswordChangeForm(data=request.POST, user=request.user)
+    if request.method == 'POST':
+        form = PasswordChangeForm(data=request.POST, user=request.user)
         if form.is_valid():
             form.save()
-            update_session_auth_hash(request,form.user)
+            update_session_auth_hash(request, form.user)
             return redirect('/home/')
         else:
             return redirect('/change_password/')
     else:
-        form=PasswordChangeForm(user=request.user)
-        return render(request,'change_password.html',{
-            'form':form,
+        form = PasswordChangeForm(user=request.user)
+        return render(request, 'change_password.html', {
+            'form': form,
         })
