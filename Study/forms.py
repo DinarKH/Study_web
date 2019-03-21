ffc474b45c781b4e318a54b65d528177c1fee87d
Dinar5 <>
2019-03-21 16:06:43 +0400

    login template

--- Study/forms.py
+++ Study/forms.py
@@ -1,7 +1,8 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
-from .models import Lesson, Comment, CommentExample, Subject, UserProfile, Example
+from .models import Lesson, Comment, CommentExample, \
+    Subject, UserProfile, Example
 
 
 class RegistrationForm(UserCreationForm):
